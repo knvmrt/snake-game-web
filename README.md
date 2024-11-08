47fb70fde003cb49d236f98666a6a86e2b95d72b
Murad KnV <knvmrt@gmail.com>
2024-11-08 16:56:09 +0400

    Sneke Game 2

---
+++ README.md
@@ -0,0 +1,20 @@
+## 🌐 Sosial Medias:
+
+[![LinkedIn](https://img.shields.io/badge/LinkedIn-%230077B5.svg?style=for-the-badge&logo=linkedIn&logoColor=white)](https://linkedin.com/in/knvmrt)
+[![GITHUB](https://img.shields.io/badge/GitHub-%23081030.svg?style=for-the-badge&logo=github&logoColor=white)](https://github.com/knvmrt)
+[![GITLAB](https://img.shields.io/badge/GitLab-%23E1442A.svg?style=for-the-badge&logo=gitlab&logoColor=white)](https://gitlab.com/knvmrt)
+[![Instagram](https://img.shields.io/badge/Instagram-%23E4405F.svg?style=for-the-badge&logo=instagram&logoColor=white)](https://instagram.com/knvmrt)
+[![Behance](https://img.shields.io/badge/Behance-1769ff.svg?style=for-the-badge&logo=behance&logoColor=white)](https://behance.net/knvmrt)
+[![Bento](https://img.shields.io/badge/Bento-%23000000.svg?style=for-the-badge&logo=bento&logoColor=white)](https://bento.me/knvmrt)
+
+<hr></hr>
+
+# 🗂️Project information:
+### Project Name: Snake Game 2
+### Project Description: Snake Game Web 
+
+## 💻Project tech:
+
+![HTML5](https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white)
+![CSS](https://img.shields.io/badge/css-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white)
+![JavaScript](https://img.shields.io/badge/java%20script-%23223300.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E)
---
+++ src/scripts/js/index.js
@@ -0,0 +1,110 @@
+const canvas = document.getElementById("gameCanvas");
+const ctx = canvas.getContext("2d");
+const startButton = document.getElementById("startButton");
+const box = 20; 
+
+canvas.width = 40 * box;
+canvas.height = 40 * box;
+
+let snake;
+let direction;
+let food;
+let score;
+let game;
+
+document.addEventListener("keydown", event => {
+    if (event.key === "ArrowUp" && direction !== "DOWN") direction = "UP";
+    else if (event.key === "ArrowDown" && direction !== "UP") direction = "DOWN";
+    else if (event.key === "ArrowLeft" && direction !== "RIGHT") direction = "LEFT";
+    else if (event.key === "ArrowRight" && direction !== "LEFT") direction = "RIGHT";
+});
+
+function isGameOver(head, snakeArray) {
+    if (head.x < 0 || head.y < 0 || head.x >= canvas.width || head.y >= canvas.height) {
+        return true;
+    }
+    for (let i = 0; i < snakeArray.length; i++) {
+        if (head.x === snakeArray[i].x && head.y === snakeArray[i].y) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function generateFood() {
+    let foodX = Math.floor(Math.random() * (canvas.width / box)) * box;
+    let foodY = Math.floor(Math.random() * (canvas.height / box)) * box;
+
+    while (isFoodOnSnake(foodX, foodY)) {
+        foodX = Math.floor(Math.random() * (canvas.width / box)) * box;
+        foodY = Math.floor(Math.random() * (canvas.height / box)) * box;
+    }
+    return { x: foodX, y: foodY };
+}
+
+function isFoodOnSnake(foodX, foodY) {
+    for (let i = 0; i < snake.length; i++) {
+        if (snake[i].x === foodX && snake[i].y === foodY) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    for (let i = 0; i < snake.length; i++) {
+        ctx.fillStyle = i === 0 ? "yellow" : "green";
+        ctx.fillRect(snake[i].x, snake[i].y, box, box);
+    }
+
+    ctx.fillStyle = "red";
+    ctx.beginPath();
+    ctx.arc(food.x + box / 2, food.y + box / 2, box / 2, 0, Math.PI * 2); 
+    ctx.fill();
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if (direction === "UP") snakeY -= box;
+    if (direction === "DOWN") snakeY += box;
+    if (direction === "LEFT") snakeX -= box;
+    if (direction === "RIGHT") snakeX += box;
+
+    if (snakeX === food.x && snakeY === food.y) {
+        food = generateFood();
+        score++;
+    } else {
+        snake.pop();
+    }
+
+    const newBody = { x: snakeX, y: snakeY };
+
+    if (isGameOver(newBody, snake)) {
+        clearInterval(game);
+        alert(`Game over! Score: ${score}`);
+        resetGame();
+    }
+    snake.unshift(newBody);
+}
+
+function startGame() {
+    snake = [{ x: 14 * box, y: 15 * box }]; 
+    direction = "RIGHT"; 
+    food = generateFood(); 
+    score = 0;
+
+    game = setInterval(draw, 100);
+}
+
+function resetGame() {
+    canvas.width = 40 * box;
+    canvas.height = 40 * box;
+    startButton.disabled = false;
+}
+
+startButton.addEventListener("click", function () {
+    startButton.disabled = true;
+    startGame();
+});
